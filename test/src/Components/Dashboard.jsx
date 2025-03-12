6eb3a66106527513cb795d6c2a7a6c6bd76b0c40
soni-790 <sonikumari979817@gmail.com>
2025-03-12 17:22:12 +0530

    Update Dashboard.jsx

--- test/src/Components/Dashboard.jsx
+++ test/src/Components/Dashboard.jsx
@@ -1,23 +1,25 @@
-import React , {useState } from "react";
+import React, { useState } from "react";
 import UserCard from "./UserCard";
 
 const Dashboard = () => {
     const [users] = useState([
-      { name: " Sonam ", email: "sonam456@gmail.com", age: 17},
-      { name: "Kajal", email: "kaju789@gmail.com", age: 19 },
-      { name: "Poonam", email: "poonam123@gmail.com", age: 21 },
-      { name: "Puja", email: "puja123@gmail.com", age: 23 },
-      { name: "Prity", email: "prity456@gmail.com", age: 21 },
+      { name: "sonu", email: "sonu123@gmail.com", age: 15 },
+      { name: "kaju", email: "kaju456@gmail.com", age: 16},
+      { name: "mannu", email: "mannu789@gmail.com", age: 17 },
+      { name: "annu", email: "annu567@gmail.com", age: 18 },
+      { name: "poonam", email: "poonam777@gmail.com", age: 19 },
     ]);
   
-    return(
+    return (
       <div className="p-6">
         <h2 className="text-2xl font-bold mb-4">User Dashboard</h2>
-        <div className="flex flex-wrap gap-4 justify-center">{users.map((user,index) => (<UserCard key = {index} {...user}/>
-        ))}
+        <div className="flex flex-wrap gap-4 justify-center">
+          {users.map((user, index) => (
+            <UserCard key={index} {...user} />
+          ))}
         </div>
       </div>
     );
   };
-   
-  export default Dashboard;
+
+export default Dashboard;
